94bd7329b351c1858febf4397c29101e8521d4ef
Mikhail [azalio] Petrov <azalio@azalio.net>
2024-08-30 17:23:26 +0300

    out_kinesis: add some tests

--- tests/runtime/out_kinesis.c
+++ tests/runtime/out_kinesis.c
@@ -1,6 +1,8 @@
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 #include <fluent-bit.h>
 #include "flb_tests_runtime.h"
+#include "kinesis.h"
+
 
 /* Test data */
 #include "data/td/json_td.h" /* JSON_TD */
@@ -188,6 +190,52 @@ void flb_test_firehose_nonsense_error(void)
     unsetenv("TEST_PUT_RECORDS_ERROR");
 }
 
+void flb_test_kinesis_default_port(void)
+{
+    int ret;
+    flb_ctx_t *ctx;
+    int in_ffd;
+    int out_ffd;
+    struct flb_output_instance *out;
+
+    setenv("FLB_KINESIS_PLUGIN_UNDER_TEST", "true", 1);
+
+    /* Create context, flush every second (for testing) */
+    ctx = flb_create();
+    TEST_CHECK(ctx != NULL);
+
+    in_ffd = flb_input(ctx, (char *) "lib", NULL);
+    TEST_CHECK(in_ffd >= 0);
+    flb_input_set(ctx, in_ffd, "tag", "test", NULL);
+
+    out_ffd = flb_output(ctx, (char *) "kinesis_streams", NULL);
+    TEST_CHECK(out_ffd >= 0);
+    flb_output_set(ctx, out_ffd, "match", "*", NULL);
+    flb_output_set(ctx, out_ffd, "region", "us-west-2", NULL);
+    flb_output_set(ctx, out_ffd, "stream", "fluent", NULL);
+    flb_output_set(ctx, out_ffd, "time_key", "time", NULL);
+    /* Note: We're not setting the port explicitly */
+
+    /* Start the engine */
+    ret = flb_start(ctx);
+    TEST_CHECK(ret == 0);
+
+    /* Get the output instance */
+    out = flb_output_get_instance(ctx, out_ffd);
+    TEST_CHECK(out != NULL);
+
+    /* Get the plugin context */
+    struct flb_kinesis_streams *kinesis_ctx = (struct flb_kinesis_streams *) flb_output_get_context(out);
+    TEST_CHECK(kinesis_ctx != NULL);
+
+    /* Check that the default port is 443 */
+    TEST_CHECK(kinesis_ctx->port == FLB_KINESIS_DEFAULT_HTTPS_PORT);
+    TEST_MSG("Default port should be %d, but got %d", FLB_KINESIS_DEFAULT_HTTPS_PORT, kinesis_ctx->port);
+
+    flb_stop(ctx);
+    flb_destroy(ctx);
+}
+
 void flb_test_kinesis_custom_port(void)
 {
     int ret;
@@ -197,7 +245,6 @@ void flb_test_kinesis_custom_port(void)
 
     /* mocks calls- signals that we are in test mode */
     setenv("FLB_KINESIS_PLUGIN_UNDER_TEST", "true", 1);
-    setenv("TEST_CUSTOM_PORT", "4443", 1);
 
     ctx = flb_create();
 
@@ -222,7 +269,36 @@ void flb_test_kinesis_custom_port(void)
     sleep(2);
     flb_stop(ctx);
     flb_destroy(ctx);
-    unsetenv("TEST_CUSTOM_PORT");
+}
+
+void flb_test_kinesis_invalid_port(void)
+{
+    int ret;
+    flb_ctx_t *ctx;
+    int in_ffd;
+    int out_ffd;
+
+    setenv("FLB_KINESIS_PLUGIN_UNDER_TEST", "true", 1);
+
+    ctx = flb_create();
+
+    in_ffd = flb_input(ctx, (char *) "lib", NULL);
+    TEST_CHECK(in_ffd >= 0);
+    flb_input_set(ctx, in_ffd, "tag", "test", NULL);
+
+    out_ffd = flb_output(ctx, (char *) "kinesis_streams", NULL);
+    TEST_CHECK(out_ffd >= 0);
+    flb_output_set(ctx, out_ffd, "match", "*", NULL);
+    flb_output_set(ctx, out_ffd, "region", "us-west-2", NULL);
+    flb_output_set(ctx, out_ffd, "stream", "fluent", NULL);
+    flb_output_set(ctx, out_ffd, "time_key", "time", NULL);
+    flb_output_set(ctx, out_ffd, "port", "99999", NULL);  // Invalid port
+    flb_output_set(ctx, out_ffd, "Retry_Limit", "1", NULL);
+
+    ret = flb_start(ctx);
+    TEST_CHECK(ret != 0);  // Expect failure
+
+    flb_destroy(ctx);
 }
 
 /* Test list */
@@ -232,6 +308,8 @@ TEST_LIST = {
     {"throughput_error", flb_test_firehose_throughput_error },
     {"unknown_error", flb_test_firehose_error_unknown },
     {"nonsense_error", flb_test_firehose_nonsense_error },
+    {"default_port", flb_test_kinesis_default_port },
     {"custom_port", flb_test_kinesis_custom_port },
+    {"invalid_port", flb_test_kinesis_invalid_port },
     {NULL, NULL}
 };
